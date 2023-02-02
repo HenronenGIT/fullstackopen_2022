a533fceefdd347e7c510b05db05914f10187ce20
HenriM <henrimaronen@gmail.com>
2023-02-02 11:41:34 +0200

    error handling added for the routes with express-async-error library

--- part12/controllers/blogs.js
+++ part12/controllers/blogs.js
@@ -3,59 +3,38 @@ const router = require('express').Router()
 const { response } = require('express')
 const { Blog } = require('../models')
 
+// Get all the blogs
 router.get('/', async (req, res) => {
 	const blogs = await Blog.findAll()
-	res.json(blogs)
+	return res.status(200).json(blogs)
 })
 
 // Add new blog
 router.post('/', async (req, res) => {
-	try {
-		console.log(req.body)
-		const blog = await Blog.create(req.body)
-		res.json(blog)
-	}
-	catch (e) {
-		return res.status(400).json({ error })
-	}
+	const blog = await Blog.create(req.body)
+	res.status(202).json(blog)
 })
 
 // Deleting one blog
-router.delete('/api/blogs/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
 	const deletedRows = await Blog.destroy({
 		where: {
 			id: req.params.id
 		}
 	})
-	res.json({ deletedRows: deletedRows })
+	return res.status(204).json({ "deletedRows": deletedRows })
 })
 
-// Updating likes
+// Updating likes of the single blog
 router.put('/:id', async (req, res) => {
 	body = req.body
 
-	try {
-		const blogToUpdate = await Blog.findByPk(req.params.id);
-		const updatedBlog = await blogToUpdate.update({
-			likes: body.likes
-		})
-		console.log(`Blog with the id ${req.params.id} has been updated`)
-		return res.status(200).send(updatedBlog)
-	} catch (e) {
-		console.log(`Blog with id [${req.params.id}] not found`)
-		res.status(404).end()
-	}
-
-	// const blogToUpdate = await Blog.findByPk(req.params.id);
-	// if (!blogToUpdate) {
-	// 	console.error(`Blog with id ${req.params.id} not found.`);
-	// 	res.status(404).end()
-	// }
-	// const updatedBlog = await blogToUpdate.update({
-	// 	likes: body.likes
-	// })
-	// console.log(`Blog with the id ${req.params.id} has been updated`)
-	// res.status(200).end()
+	const blogToUpdate = await Blog.findByPk(req.params.id);
+	const updatedBlog = await blogToUpdate.update({
+		likes: body.likes
+	})
+	console.log(`Blog with the id ${req.params.id} has been updated`)
+	return res.status(200).send(updatedBlog)
 })
 
 module.exports = router
--- part12/index.js
+++ part12/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('express-async-errors');
 
 const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
