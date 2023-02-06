54a147733072eec895724319ae84f4076ed3be4f
HenriM <henrimaronen@gmail.com>
2023-02-06 12:22:22 +0200

    new script added to package json

---
+++ part12/migrations/20230106_00_initialize_blogs_and_users.js
@@ -0,0 +1,77 @@
+const { DATE } = require('sequelize')
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+	up: async ({ context: queryInterface }) => {
+		await queryInterface.createTable('blogs', {
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			author: {
+				type: DataTypes.TEXT,
+				allowNull: false
+			},
+			url: {
+				type: DataTypes.TEXT
+			},
+			title: {
+				type: DataTypes.TEXT
+			},
+			likes: {
+				type: DataTypes.INTEGER,
+				defaultValue: 0
+			},
+		})
+		// await queryInterface.createTable('users', {
+		// 	id: {
+		// 		type: DataTypes.INTEGER,
+		// 		primaryKey: true,
+		// 		autoIncrement: true
+		// 	},
+		// 	name: {
+		// 		type: DataTypes.STRING,
+		// 		allowNull: false,
+		// 		validate: {
+		// 			notEmpty: {
+		// 				args: true,
+		// 				msg: '[name] cannot be empty'
+		// 			},
+		// 		}
+		// 	},
+		// 	username: {
+		// 		type: DataTypes.STRING,
+		// 		allowNull: false,
+		// 		validate: {
+		// 			notEmpty: {
+		// 				args: true,
+		// 				msg: '[username] cannot be empty'
+		// 			},
+		// 			isEmail: {
+		// 				isEmail: {
+		// 					args: true,
+		// 					msg: "Validation isEmail on username failed"
+		// 				}
+		// 			}
+		// 		}
+		// 	},
+		// })
+		await queryInterface.addColumn('blogs', 'user_id', {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: { model: 'users', key: 'id' },
+		})
+		// await queryInterface.addColumn('blogs', 'created_at', {
+		// 	// type: DATE
+		// })
+		// await queryInterface.addColumn('blogs', 'updated_at', {
+		// 	// type: DATE
+		// })
+
+	},
+	down: async ({ context: queryInterface }) => {
+		await queryInterface.dropTable('blogs')
+		await queryInterface.dropTable('users')
+	},
+}
