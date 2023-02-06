1a013ca2ded56419d8ea4cf05e5a11671991f065
HenriM <henrimaronen@gmail.com>
2023-02-06 13:40:22 +0200

    migrations removed, author routing added to index.js

--- part12/index.js
+++ part12/index.js
@@ -8,12 +8,14 @@ const { connectToDatabase } = require('./util/db')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorsRouter = require('./controllers/authors')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/authors', authorsRouter)
 
 const start = async () => {
 	await connectToDatabase()
