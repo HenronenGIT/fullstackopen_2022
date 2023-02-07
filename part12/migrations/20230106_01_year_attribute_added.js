cc96dfc8cdcd14194f78a3a77637d13eb25ad24a
HenriM <henrimaronen@gmail.com>
2023-02-07 15:42:28 +0200

    13.18 - year attribute added to blog, working like it should b

--- part12/migrations/20230106_01_year_attribute_added.js
+++ part12/migrations/20230106_01_year_attribute_added.js
@@ -2,20 +2,18 @@ const { DataTypes } = require('sequelize')
 
 module.exports = {
 	up: async ({ context: queryInterface }) => {
-		await queryInterface.addColumn(
-			'blogs',
-			'year',
-			{
-				type: DataTypes.INTEGER,
-				allowNull: false,
-				validate: {
-					min: 1991,
-					max: new Date().getFullYear()
-				}
+		await queryInterface.addColumn('blogs', 'year', {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				min: 1991,
+				max: new Date().getFullYear()
 			}
+		}
 		)
 	},
 	down: async ({ context: queryInterface }) => {
 		await queryInterface.removeColumn('blogs', 'year')
 	},
+
 }
