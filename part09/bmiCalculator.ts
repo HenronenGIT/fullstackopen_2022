d319c92f7a1d6b31ec670a7bdfa4bd8c6a8d946f
Henri Maronen <henri.maronen@slush.org>
2024-01-06 11:29:18 +0200

    part09: 9.5 done

--- part09/bmiCalculator.ts
+++ part09/bmiCalculator.ts
@@ -1,24 +1,35 @@
-const calculateBmi = (height: number, weight: number): string => {
+interface returnObject {
+  weight: number;
+  height: number;
+  result: string;
+}
+
+const calculateBmi = (height: number, weight: number): returnObject => {
   if (isNaN(height) || isNaN(weight)) {
     throw new Error("Provided values were not numbers!");
   }
 
-  if (height === 0 || weight === 0) throw new Error("Height or weight cannot be 0!");
+  if (height === 0 || weight === 0)
+    throw new Error("Height or weight cannot be 0!");
+
   const bmi = weight / (((height / 100) * height) / 100);
 
+  let result = "";
   if (bmi <= 18.4) {
-    return "Underweight";
+    result = "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    result = "Normal (healthy weight)";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    result = "Overweight";
+  } else {
+    result = "Obese";
   }
-  if (bmi >= 18.5 && bmi <= 24.9) {
-    return "Normal (healthy weight)";
-  }
-  if (bmi >= 25 && bmi <= 29.9) {
-    return "Overweight";
-  }
-  return "Obese";
+  return { weight, height, result };
 };
 
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
+// const height: number = Number(process.argv[2]);
+// const weight: number = Number(process.argv[3]);
+//
+// console.log(calculateBmi(height, weight));
 
-console.log(calculateBmi(height, weight));
+export default calculateBmi;
--- part09/index.ts
+++ part09/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import calculateBmi from "./bmiCalculator";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (_req, res) => {
+  if (!_req.query.height || !_req.query.weight) {
+    res.send({ error: "malformatted parameters" });
+  }
+
+  const height: number = Number(_req.query.height);
+  const weight: number = Number(_req.query.weight);
+
+  const bmi = calculateBmi(height, weight);
+  res.send(bmi);
 });
 
 const PORT = 3001;
