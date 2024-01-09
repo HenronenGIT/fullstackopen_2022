f13a0a581aec837f9dc3608d5f72849e21639a42
Henri Maronen <henri.maronen@slush.org>
2024-01-09 20:31:04 +0200

    9.19

--- part09/flight-diary-ui/src/App.tsx
+++ part09/flight-diary-ui/src/App.tsx
@@ -37,11 +37,10 @@ const App = () => {
       })
       .catch((error) => {
         if (axios.isAxiosError(error)) {
-          setNotification(error.response.data);
+          setNotification(error.response?.data);
           setTimeout(() => {
             setNotification(null);
           }, 5000);
-          // Do something with this error...
         } else {
           console.error(error);
         }
@@ -77,32 +76,110 @@ const App = () => {
           <form onSubmit={handleSubmit}>
             <label htmlFor="name">Date:</label>
             <input
-              type="text"
-              id="name"
-              name="name"
+              type="date"
+              id="date"
+              name="date"
               value={newDiary.date}
               onChange={handleDateChange}
             />
 
-            <label htmlFor="visibility">Visibility:</label>
-            <input
-              type="text"
-              id="visibility"
-              name="visibility"
-              onChange={handleVisibilityChange}
-            />
+            <fieldset>
+              <legend>Visibility:</legend>
+              <label>
+                <input
+                  type="radio"
+                  name="visibility"
+                  value="great"
+                  onChange={handleVisibilityChange}
+                />
+                Great
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="visibility"
+                  value="good"
+                  onChange={handleVisibilityChange}
+                />
+                Good
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="visibility"
+                  value="ok"
+                  onChange={handleVisibilityChange}
+                />
+                OK
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="visibility"
+                  value="poor"
+                  onChange={handleVisibilityChange}
+                />
+                Poor
+              </label>
+            </fieldset>
 
-            <label htmlFor="weather">Weather:</label>
-            <input
-              type="text"
-              id="weather"
-              name="weather"
-              onChange={handleWeatherChange}
-            />
+            <div>
+              <fieldset>
+                <legend>Weather:</legend>
+                <label>
+                  <input
+                    type="radio"
+                    name="weather"
+                    value="sunny"
+                    onChange={handleWeatherChange}
+                  />
+                  Sunny
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="weather"
+                    value="rainy"
+                    onChange={handleWeatherChange}
+                  />
+                  Rainy
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="weather"
+                    value="cloudy"
+                    onChange={handleWeatherChange}
+                  />
+                  Cloudy
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="weather"
+                    value="stormy"
+                    onChange={handleWeatherChange}
+                  />
+                  Stormy
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="weather"
+                    value="windy"
+                    onChange={handleWeatherChange}
+                  />
+                  Windy
+                </label>
+              </fieldset>
+            </div>
 
             <label htmlFor="comment">Comment:</label>
             <input id="comment" name="comment" onChange={handleCommentChange} />
-            <button type="submit">Submit</button>
+            <div>
+              <button type="submit">Submit</button>
+              <button type="reset">Reset</button>
+            </div>
           </form>
         </div>
       </div>
