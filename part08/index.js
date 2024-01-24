09253ae65c31677a600ac55c0af986bde804df47
Henri Maronen <henri.maronen@slush.org>
2024-01-24 10:50:03 +0200

    part 08 - 8.5 done

--- part08/index.js
+++ part08/index.js
@@ -95,7 +95,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -122,6 +122,9 @@ const resolvers = {
     allBooks: (_, args) => {
       if (args.author) {
         return books.filter((book) => book.author === args.author);
+      }
+      if (args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre));
       }
       return books;
     },
